36bb307299784f08826ac91fb5527745490e86bf
Charlie Ylijoki <ylijokic@oregonstate.edu>
2019-12-26 23:16:34 -0800

    Node & Gatsby Setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Charlie's Blog`,
+    author: `Charlie Ylijoki`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
