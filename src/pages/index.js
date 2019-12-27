be4dcff981ff0d78cc5e8d815d3bf6fb910678ba
Charlie Ylijoki <ylijokic@oregonstate.edu>
2019-12-26 23:15:28 -0800

    Create Website Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+      <p>I'm Charlie and this is my website!</p>
+    </Layout>
+  )
+}
+
+export default IndexPage
