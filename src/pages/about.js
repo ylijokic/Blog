bc97300d317bfb0623e1ca9cfc3639954f9e1ec3
Charlie <ylijokic@oregonstate.edu>
2020-07-20 21:37:18 -0700

    Update About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,17 +13,22 @@ const AboutPage = () => {
         <p>My name is Charlie.</p>
       </div>
       <p>
-        I am a Computer Science student at Oregon State University.
+        I am a Computer Science student at Oregon State University and I am
+        interested in Full Stack Web Development and Data Analytics.
         <br />
         <br />
-        My background is related to the environmental field, where I initially
-        became interested in programming while learning about Geographic
-        Information Systems (GIS). This led me to pursue a second Bachelor's
-        degree in Computer Science.
+        My academic background is related to the environmental field, where I
+        initially became interested in programming while learning about
+        Geographic Information Systems (GIS). This ultimately led me to pursue a
+        second Bachelor's degree in Computer Science.
         <br />
-        <br />
-        My interests are focused on full stack development and I am currently
-        looking for internship opportunities to expand on my technical skills.
+        <br />I love Computer Science because it is a field of study that has
+        endless learning opportunities. It's amazing to be able to learn new
+        technologies and apply my personal interests to be able to create unique
+        applications. I am also inspired to learn about how big data analytics
+        can help to drive innovation. Take a look at some of my projects and
+        blog posts and reach out if you think I would be a good fit for your
+        team. Thank you for visiting my website!
       </p>
     </Layout>
   )
